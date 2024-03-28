409dc92d4d8179cf72de8d21e9fe422109478285
Marvin Kabira <marvobd@gmail.com>
2024-03-28 17:01:28 +0300

    completed the necesarry additions to the index.js file.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// or we could simply write the above code as:
+// const excitedWelcomeMessage = currentUser.toUpperCase();
+
+const firstInitial = currentUser[0]
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+// we could also use:
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+// when using the above line of code, code line 7 would not be necessary.
